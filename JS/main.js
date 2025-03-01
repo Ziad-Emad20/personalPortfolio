bfbbae0954922b4c861c1018f8dfe3715338ff50
Ziad Emad <ziaemad0111@gmail.com>
2025-03-01 17:54:00 +0200

    Responsive for all

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,54 @@
- var swiper = new Swiper(".slide-content", {
+let menuIcon = document.querySelector('#icon-menu');
+let nav = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(section => {
+    let top = window.scrollY;
+    let sectionTop = section.offsetTop - 150;
+    let sectionHeight = section.offsetHeight;
+
+    let id = section.getAttribute('id');
+    if (top >= sectionTop && top < sectionTop + sectionHeight){
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${id}`) {
+          link.classList.add('active');
+        }
+      });
+    }
+  })
+};
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('icon-close-outline');
+  nav.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".slide-content", {
       slidesPerView: 3,
       centeredSlides: true,
       spaceBetween: 30,
@@ -15,7 +65,7 @@
         0: {
             slidesPerView: 1,
         },
-        800: {
+        768: {
             slidesPerView: 2,
         },
         1200: {
